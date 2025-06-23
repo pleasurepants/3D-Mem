60579a4c0c834ea9b12231b1a78fdf05241ee123
gengyuanmax <gengyuanmax@gmail.com>
2025-06-23 12:26:13 +0000

    qwen

--- script/baseline_qwen.sh
+++ script/baseline_qwen.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=qwen_bl
+#SBATCH --job-name=q_bl
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=10-00:00:00
-#SBATCH --output=/home/wiss/zhang/code/openeqa/3D-Mem/slurm/baseline/qwen_baseline-%j.out
+#SBATCH --exclude=worker-minor-1,worker-minor-3,worker-minor-4,worker-minor-5,worker-minor-6,worker-3,worker-4,worker-8,worker-9,worker-1,worker-2
+#SBATCH --output=/home/wiss/zhang/code/openeqa/3D-Mem/slurm/qwen/baseline-%j.out
 #SBATCH --partition all
 
 echo "=== JOB START ==="
@@ -13,6 +14,7 @@ date
 hostname
 nvidia-smi
 echo "SLURM_JOB_ID: $SLURM_JOB_ID"
+export LD_LIBRARY_PATH=/home/wiss/zhang/local_cuda118/cuda_cudart/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
 
 
 if [ -z "$SLURM_JOB_GPUS" ]; then
@@ -31,20 +33,20 @@ CUDA_VISIBLE_DEVICES=0 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
 vllm serve Qwen/Qwen2-VL-7B-Instruct \
     --served-model-name qwen \
     --port 8000 \
-    --limit-mm-per-prompt image=50 &
-
+    --limit-mm-per-prompt image=20 &
+    # --trust-remote-code &
 VLLM_PID=$!
 
 
 echo "[INFO] Waiting for vLLM (qwen) server to be ready..."
-for i in {1..120}; do
+for i in {1..300}; do
     if curl -s http://localhost:8000/v1/models > /dev/null; then
         echo "[INFO] ✅ qwen API is ready!"
         break
     fi
-    echo "  ... waiting ($((i*2))s)"
-    sleep 2
-    if [ $i -eq 120 ]; then
+    echo "  ... waiting ($((i*10))s)"
+    sleep 10
+    if [ $i -eq 300 ]; then
         echo "[ERROR] ❌ Timeout: qwen server failed to start."
         if [ -n "$VLLM_PID" ] && kill -0 "$VLLM_PID" 2>/dev/null; then
             kill "$VLLM_PID"
