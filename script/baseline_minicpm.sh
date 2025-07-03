fc3fb49dd5fd8a072ed62bccf83bace826d2cbdb
gengyuanmax <gengyuanmax@gmail.com>
2025-07-03 17:44:18 +0000

    1

--- script/baseline_minicpm.sh
+++ script/baseline_minicpm.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=10-00:00:00
-#SBATCH --exclude=worker-minor-1,worker-minor-3,worker-minor-4,worker-minor-5,worker-minor-6,worker-3,worker-4,worker-8,worker-9,worker-7
+#SBATCH --exclude=worker-minor-1,worker-minor-3,worker-minor-4,worker-minor-5,worker-minor-6,worker-3,worker-4,worker-8
 #SBATCH --output=/home/wiss/zhang/code/openeqa/3D-Mem/slurm/minicpm/context_baseline-%j.out
 #SBATCH --partition all
 
