29b1673919372eecc370e84aba39aa4caeaa446b
gengyuanmax <gengyuanmax@gmail.com>
2025-07-03 17:09:26 +0000

    1

--- src/const.py
+++ src/const.py
@@ -2,7 +2,7 @@
 INVALID_SCENE_ID = []
 import os
 # about chatgpt api
-END_POINT = "http://localhost:8001/v1/"
+END_POINT = "http://localhost:8000/v1/"
 OPENAI_KEY = os.getenv("OPENAI_API_KEY")
 
 # # about habitat scene
