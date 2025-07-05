a598250ff54609dcace46621c4bab975065e3734
gengyuanmax <gengyuanmax@gmail.com>
2025-07-05 23:14:51 +0200

    1

--- src/const.py
+++ src/const.py
@@ -1,13 +1,13 @@
-# INVALID_SCENE_ID = []
-# import os
-# # about chatgpt api
-# END_POINT = "http://localhost:8000/v1/"
-# OPENAI_KEY = os.getenv("OPENAI_API_KEY")
-
-
-
 INVALID_SCENE_ID = []
 import os
 # about chatgpt api
-END_POINT = "https://api.openai.com/v1/"
+END_POINT = "http://localhost:8000/v1/"
 OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+
+
+
+# INVALID_SCENE_ID = []
+# import os
+# # about chatgpt api
+# END_POINT = "https://api.openai.com/v1/"
+# OPENAI_KEY = os.getenv("OPENAI_API_KEY")
