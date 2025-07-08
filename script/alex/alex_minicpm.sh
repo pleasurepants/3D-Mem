3640ab9fd7e162bda31bf8e4738561b069634393
gengyuanmax <gengyuanmax@gmail.com>
2025-07-08 22:58:34 +0200

    1

--- script/alex/alex_minicpm.sh
+++ script/alex/alex_minicpm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=m-l-c-o
+#SBATCH --job-name=m-l-c-s
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:a40:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=24:00:00 
-#SBATCH --output=/home/hpc/v100dd/v100dd12/code/3D-Mem/slurm/minicpm/list-con-only-%j.out 
+#SBATCH --output=/home/hpc/v100dd/v100dd12/code/3D-Mem/slurm/minicpm/list-con-score-%j.out 
 #SBATCH --partition a40
 
 
