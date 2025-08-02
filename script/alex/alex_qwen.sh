6e5aed01e8193f3315c3228b1e69d20ed15d16a0
gengyuanmax <gengyuanmax@gmail.com>
2025-08-02 20:11:01 +0200

    seed

--- script/alex/alex_qwen.sh
+++ script/alex/alex_qwen.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --time=24:00:00 
-#SBATCH --output=/home/hpc/v100dd/v100dd12/code/3D-Mem/slurm/lifelong/qwen/embedding-lifelong-cotv1-%j.out
+#SBATCH --output=/home/hpc/v100dd/v100dd12/code/3D-Mem/slurm/lifelong/qwen/embedding-lifelong-cot-seed66-%j.out
 #SBATCH --partition a100
 
 
