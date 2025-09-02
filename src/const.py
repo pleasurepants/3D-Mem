3ae720d417efc141908db3061009ca590fe383c5
Jingpei <jingpei.wu@dbs.ifi.lmu.de>
2025-09-02 19:49:38 +0200

    adapt to dbs settings

--- src/const.py
+++ src/const.py
@@ -2,8 +2,12 @@
 INVALID_SCENE_ID = []
 import os
 # about chatgpt api
-END_POINT = "http://localhost:8000/v1/"
-OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+# END_POINT = "http://localhost:8000/v1/"
+# OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+END_POINT = "http://localhost:8009/v1"
+# END_POINT = "http://10.153.51.155:8009/v1"   # worker-6
+OPENAI_KEY = "dummy-key"
+
 
 # # about habitat scene
 # INVALID_SCENE_ID = []
