730af71b39a404662fd59948d6fcc8fee34de4f2
Jingpei <jingpei.wu@dbs.ifi.lmu.de>
2025-09-06 19:14:35 +0200

    debug

--- src/eval_utils_gpt_goatbench.py
+++ src/eval_utils_gpt_goatbench.py
@@ -45,7 +45,8 @@ def call_openai_api(sys_prompt, contents) -> Optional[str]:
     while retry_count < max_tries:
         try:
             completion = client.chat.completions.create(
-                model="gpt-4o",  # model = "deployment_name"
+                # model="gpt-4o",  # model = "deployment_name"
+                model="qwen",
                 messages=message_text,
                 temperature=0.7,
                 max_tokens=4096,
