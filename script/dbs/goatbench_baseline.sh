c3bff80e2e296c6fc2cfe9e9d2b0f6d9cc2bf20f
Jingpei <jingpei.wu@dbs.ifi.lmu.de>
2025-10-27 20:42:53 +0000

    update files

--- script/dbs/goatbench_baseline.sh
+++ script/dbs/goatbench_baseline.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=goatbench_baseline_no-image-limit_seed19
+#SBATCH --job-name=goatbench_hierarchical-cot_seed13
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=8
 #SBATCH --time=2-00:00:00
-#SBATCH --nodelist=worker-9
+#SBATCH --nodelist=worker-3
 #SBATCH --output=/nfs/data8/jingpei/eqa/3D-Mem/slurm/goatbench/%x-%j.out 
 
 
 source /home/wiss/jingpei/anaconda3/bin/activate 
+# conda activate unsloth
+# vllm serve Qwen/Qwen2.5-VL-7B-Instruct --served-model-name qwen --port 8009 --max-model-len 100000 --seed 13
 conda activate 3dmem
 
 date
@@ -20,7 +22,8 @@ which python
 
 cd /nfs/data8/jingpei/eqa/3D-Mem
 
-export END_POINT="http://10.153.51.155:8008/v1"
+export END_POINT="http://10.153.51.155:8009/v1"    # worker-6
 
 
-python run_goatbench_evaluation.py -cf cfg/dbs_cfg/goatbench_qwen_13.yaml
+python run_goatbench_evaluation.py -cf cfg/dbs_cfg/goatbench_qwen_seed.yaml
+# python run_goatbench_evaluation_qwen.py -cf cfg/dbs_cfg/goatbench_qwen_13.yaml --replay_top 0
--- script/dbs/goatbench_hierarchical-cot_rollout.sh
+++ script/dbs/goatbench_hierarchical-cot_rollout.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=goatbench_hierarchical-cot_seed19
+#SBATCH --job-name=goatbench_val-unseen-2_hierarchical-cot
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=10-00:00:00
-#SBATCH --nodelist=worker-3
+#SBATCH --nodelist=worker-7
 #SBATCH --output=/nfs/data8/jingpei/eqa/3D-Mem/slurm/goatbench/%x-%j.out 
 
 
@@ -22,8 +22,10 @@ cd /nfs/data8/jingpei/eqa/3D-Mem
 
 # export END_POINT="http://10.153.51.155:8009/v1"    # worker-6
 # export END_POINT="http://10.153.51.154:8009/v1"    # worker-5
-export END_POINT="http://10.153.51.154:8008/v1"    # worker-5
+export END_POINT="http://10.153.51.154:8006/v1"    # worker-5
 
 
-# python run_goatbench_evaluation.py -cf cfg/dbs_cfg/goatbench_qwen_13.yaml
-python run_goatbench_evaluation_qwen.py -cf cfg/dbs_cfg/goatbench_qwen_seed.yaml --replay_top 0
+# training set
+# python run_goatbench_evaluation_qwen.py -cf cfg/dbs_cfg/goatbench_qwen_rollout.yaml --replay_top 0
+# val-unseen set
+python run_goatbench_evaluation_qwen.py -cf cfg/dbs_cfg/goatbench_qwen_rollout.yaml --replay_top 0 --split 2
